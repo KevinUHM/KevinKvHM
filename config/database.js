3669baab6a9fe489a170ffd127bb555bc7f1f0cd
KevinKvHM <77523716+KevinKvHM@users.noreply.github.com>
2022-10-18 16:10:01 -0500

    prueba-server3

--- config/database.js
+++ config/database.js
@@ -5,7 +5,8 @@ module.exports = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
+    //"host": "127.0.0.1",
     dialect: process.env.DB_DIALECT || "postgres", 
   
   /*
