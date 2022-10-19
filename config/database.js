c5fdeb7ac96b5809a0093cdac6c521a2e28fd977
KevinKvHM <hernandezku.103@gmail.com>
2022-10-19 16:55:43 -0500

    cambios

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    "username": "postgres",
-    "password": "1234",
-    "database": "prueba",
-    "host": "127.0.0.1",
-    "port": 5432,
+    username: "postgres",
+    password: "1234",
+    database: "prueba",
+    host: "127.0.0.1",
+    port: 5432,
     dialect: "postgres",
   },
   production: {
@@ -15,7 +15,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      dialect: process.env.DB_DIALECT || "postgres",
+      dialect: "postgres",
   }
 };
   /*
