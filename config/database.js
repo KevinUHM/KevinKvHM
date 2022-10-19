8abcd494d7e5e5f25aa41da48032702eee088a29
KevinKvHM <77523716+KevinKvHM@users.noreply.github.com>
2022-10-19 14:57:06 -0500

    cambiosParaServer1

--- config/database.js
+++ config/database.js
@@ -1,26 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-   
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+  development: {
+    "username": "postgres",
+    "password": "1234",
+    "database": "prueba",
     "host": "127.0.0.1",
-    dialect: process.env.DB_DIALECT || "postgres", 
-  
-  /*
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_DATABASE || "prueba",
-  host: process.env.DB_HOST || "127.0.0.1",
-  dialect: process.env.DB_DIALECT || "postgres", 
-  define: {
-    timestamps: false,
-
-    //Genera claves foraneas de tipo model_id en lugar de modelId
-    udescored: true
-   
-    
-  }*/
-  
-}
+    "port": 5432,
+    dialect: "postgres",
+  },
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: process.env.DB_DIALECT,
+  }
+};
