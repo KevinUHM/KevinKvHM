b77d146c58c8132799e5871f4a32c19c192e322e
KevinKvHM <77523716+KevinKvHM@users.noreply.github.com>
2022-10-19 18:29:57 -0500

    actu

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      dialect: "postgres",
+      dialect: "postgres"
   }
   //nuevps cambios
 };
